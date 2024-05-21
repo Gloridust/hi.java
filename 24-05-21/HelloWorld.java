9a87f9cf8dae4fd9512a0244e7354765b263aeab
Gloridust <gloridust@gmail.com>
2024-05-21 20:34:26 +0800

    helloworld

---
+++ 24-05-21/HelloWorld.java
@@ -0,0 +1,10 @@
+// HelloWorld.java
+
+// 定义一个名为HelloWorld的类
+public class HelloWorld {
+    // 主方法，程序入口
+    public static void main(String[] args) {
+        // 输出"Hello, World!"
+        System.out.println("Hello, World!");
+    }
+}
